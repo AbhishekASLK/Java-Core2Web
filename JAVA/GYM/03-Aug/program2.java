07a42e2a6046fc62d547fcb6fd6e2dfe0e1804c7
AbhishekASLK <bhoslea925@gmail.com>
2023-08-03 12:26:09 +0530

    work-out

---
+++ JAVA/GYM/03-Aug/program2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Client {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		Stack s1 = new Stack();
+		Stack s2 = new Stack();
+
+		synchronized(s1){
+		
+			// code
+			//
+			//
+			//
+			//
+			//
+
+			s2.wait();
+		}
+
+	}
+}
