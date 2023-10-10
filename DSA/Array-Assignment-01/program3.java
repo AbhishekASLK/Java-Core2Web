c1e7a663d7c4af2043894cd3f3dc8121e6afe4c7
AbhishekASLK <bhoslea925@gmail.com>
2023-10-10 17:19:09 +0530

    modified program3 and program6

--- DSA/Array-Assignment-01/program3.java
+++ DSA/Array-Assignment-01/program3.java
@@ -3,6 +3,22 @@ import java.util.*;
 
 class rangeSumQuery {
 
+	static int[] rangeSumQuery(int[] arr, int n, int[][] query){
+	
+		int[] res = new int[m];
+
+		// [[0,3],[1,2]]
+
+		for(int i=0;i<query.length;i++){
+			if(query[i][0] == 0)
+				res[i]=arr[query[1]];
+			else
+				res[i]=arr[query[1]]-arr[query[0]-1];
+		}
+
+		return res;
+	}
+
 	public static void main(String[] args){
 	
 		Scanner sc = new Scanner(System.in);
@@ -11,6 +27,24 @@ class rangeSumQuery {
 		for(int i=0;i<n;i++){
 			arr[i]=sc.nextInt();
 		}
-		
+		System.out.println("Enter the query count: ");
+		int m = sc.nextInt();
+		int[][] query = new int[m][2];
+		System.out.println("Enter the elements: ");
+		for(int i=0;i<m;i++){
+			query[i][0]=sc.nextInt();
+			query[i][1]=sc.nextInt();
+		}
+
+		for(int i=1;i<n;i++){
+			arr[i]+=arr[i-1];
+		}
+
+		int[] res = rangeSumQuery(arr,n,query);
+
+		for(int x:res){
+			System.out.print(x+" ");
+		}
+		System.out.println();
 	}
 }
--- DSA/Array-Assignment-01/program6.java
+++ DSA/Array-Assignment-01/program6.java
@@ -2,8 +2,39 @@ import java.util.*;
 
 class productArrayPuzzle {
 
-	static int[] productArrayPuzzle {
+	static long[] productArrayPuzzle(int[] nums, int n) {
 		
+		// [1,2,3,4,5]
+		// [120,60,40,30,24]
+
+		long[] arr = new long[n];
+        
+        	long prod = 1;
+        	int count=0;
+        	boolean flag=false;
+        	for(int i=0;i<n;i++){
+            		if(nums[i]==0){
+                		flag=true;
+                		count++;
+            		}
+           		else {
+                		prod*=nums[i];
+      			}
+        	}
+
+        	if(count>=2)
+           		return new long[n];
+            
+        	for(int i=0;i<n;i++){
+            		if(flag==true && nums[i]!=0)
+                		arr[i]=0;
+            		else if(nums[i]!=0)
+                		arr[i]=prod/nums[i];
+            		else
+                		arr[i]=prod;
+        	}
+        	
+		return arr;
 
 	}
 
@@ -15,6 +46,11 @@ class productArrayPuzzle {
 		for(int i=0;i<n;i++)
 			arr[i]=sc.nextInt();
 
-		int[] res = productArrayPuzzle(arr,n);
+		long[] res = productArrayPuzzle(arr,n);
+
+		for(long x:res){
+			System.out.print(x+" ");
+		}
+		System.out.println();
 	}
 }
